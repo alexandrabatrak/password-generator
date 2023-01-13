a0a8474bb0fa916d344a2b595e26b477a69315b7
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-13 14:00:37 +0000

    First version

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var specialCharacters = [
   '~',
   '-',
   '_',
-  '.'
+  '.',
 ];
 
 // Array of numeric characters to be included in password
@@ -55,7 +55,7 @@ var lowerCasedCharacters = [
   'w',
   'x',
   'y',
-  'z'
+  'z',
 ];
 
 // Array of uppercase characters to be included in password
@@ -85,22 +85,70 @@ var upperCasedCharacters = [
   'W',
   'X',
   'Y',
-  'Z'
+  'Z',
 ];
 
-// Function to prompt user for password options
+let rangeValueHTML = document.getElementById('rangeValue');
+let lengthInput = document.getElementById('lengthOption');
+let passwordLength = lengthInput.value;
+rangeValueHTML.innerHTML = passwordLength;
+lengthInput.oninput = function () {
+  passwordLength = lengthInput.value;
+  rangeValueHTML.innerHTML = passwordLength;
+};
+let lowercaseOption = document.getElementById('lowercaseOption');
+let uppercaseOption = document.getElementById('uppercaseOption');
+let numericOption = document.getElementById('numericOption');
+let specialOption = document.getElementById('specialOption');
+
+// create array to store user input
+// throws error./
+let userChoiceArray = [];
+// Function to get user's password options from input
 function getPasswordOptions() {
-
+  // create array to loop from
+  let options = [
+    lowercaseOption,
+    uppercaseOption,
+    numericOption,
+    specialOption,
+  ];
+  options.forEach(function (option) {
+    option.addEventListener('change', function () {
+      // dynamically adjust user input array values
+      if (option.checked) {
+        userChoiceArray.push(option.value);
+      } else {
+        userChoiceArray.splice(userChoiceArray.indexOf(option.value), 1);
+      }
+    });
+  });
 }
-
+getPasswordOptions();
+let passwordStr = '';
 // Function for getting a random element from an array
-function getRandom(arr) {
+function getRandom(userChoiceArray, passwordLength) {
+  // Note: Math.random() does not provide cryptographically secure random numbers. Do not use them for anything related to security. Use the Web Crypto API instead, and more precisely the window.crypto.getRandomValues() method.
+  // https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
+  // if (!userChoiceArray.length) {
+  //   return 'Select at least one option';
+  // }
 
+  passwordStr = '';
+  for (let i = 0; i < passwordLength; i++) {
+    let characterIndex = Math.floor(Math.random() * userChoiceArray.length);
+    let character = userChoiceArray[characterIndex];
+    passwordStr += character;
+  }
+  return passwordStr;
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+  userChoiceArray = getPasswordOptions();
+  passwordLength = document.getElementById('lengthOption').value;
+  getRandom(userChoiceArray, passwordLength);
+  return passwordStr;
 }
 
 // Get references to the #generate element
@@ -108,11 +156,28 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
+  getPasswordOptions();
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordHTML = document.querySelector('#password');
 
-  passwordText.value = password;
+  passwordHTML.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+// copy password
+// https://www.30secondsofcode.org/articles/s/copy-text-to-clipboard-with-javascript
+if (navigator && navigator.clipboard) {
+  let copyButton = document.getElementById('copyButton');
+  let password = document.querySelector('#password');
+
+  copyButton.addEventListener(
+    'click',
+    (copyToClipboard = () => {
+      navigator.clipboard.writeText(password.value);
+    })
+  );
+} else {
+  alert('Your browser does not support the Clipboard API');
+}
