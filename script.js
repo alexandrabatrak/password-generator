cc32b281eb45e0d19060b564914752ac2156b08e
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-01-17 19:40:58 +0000

    Add Fisher/Yates randomise function

--- script.js
+++ script.js
@@ -148,15 +148,29 @@ function getPasswordOptions() {
 // - update choice array values - this way it's dynamically updated when user
 // makes changes before password is generated again
 userChoiceArray = getPasswordOptions();
+// Fisher-Yates randomisation algorithm
+// https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
+function fisherYatesRandomize(arr) {
+  // Start from the last element and swap
+  // one by one. We don't need to run for
+  // the first element that's why i > 0
+  for (let i = arr.length - 1; i > 0; i--) {
+    // Pick a random index from 0 to i inclusive
+    let j = Math.floor(Math.random() * (i + 1));
+    // Swap arr[i] with the element
+    // at random index
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
 // initialise password variable
 let password = '';
 // Function for getting a random element from an array
 function getRandom() {
   // reset password for each generate round
   password = '';
+  fisherYatesRandomize(userChoiceArray);
   for (let i = 0; i < passwordLength; i++) {
-    let characterIndex = Math.floor(Math.random() * userChoiceArray.length);
-    let character = userChoiceArray[characterIndex];
+    let character = userChoiceArray[i];
     password += character;
   }
 }
